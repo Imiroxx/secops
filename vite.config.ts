e1151bd6a052cdc33916bdd7a6368d3dfdca9b04
Imiroxx <imiroxx@example.com>
2026-04-24 20:27:16 +0300

    Fix relative paths and publicDir in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -21,12 +21,13 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "client", "src"),
+      "@": path.resolve(__dirname, "client/src"),
       "@shared": path.resolve(__dirname, "shared"),
       "@assets": path.resolve(__dirname, "attached_assets"),
     },
   },
   root: ".",
+  publicDir: "client/public",
   build: {
     outDir: "dist/public",
     emptyOutDir: true,
